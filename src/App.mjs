53b358522674a1d089815864913a57050250ff09
Julian Jimenez <julianjimenez2128@gmail.com>
2025-06-27 17:55:43 -0500

    feat: Add routes for MyTripsPage and ResetPassword components

--- src/App.mjs
+++ src/App.mjs
@@ -3,7 +3,8 @@ import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import LoginPage from "./pages/LoginPage.jsx";
 import LoginForm from "./components/auth/LoginForm.jsx";
 import SignupForm from "./components/auth/SignupForm.jsx";
-
+import MyTripsPage from "./pages/MyTripsPage.jsx";
+import ResetPassword from "./components/resetPassword/ResetPassword.jsx";
 function App() {
   return (
       <BrowserRouter>
@@ -16,6 +17,8 @@ function App() {
               <Route path="login" element={<LoginForm />} />
               <Route path="signup" element={<SignupForm />} />
           </Route>
+              <Route path="/my-trips" element={<MyTripsPage />} />
+              <Route path="/reset-password/:token" element={<ResetPassword />} />
       </Routes>
       </BrowserRouter>
   );

68cd32052053169e649c95ade9a410c0a23a5619
Julian Jimenez <julianjimenez2128@gmail.com>
2025-06-27 17:57:22 -0500

    refactor: Simplify login and signup functions in authService

--- src/services/api/Auth/authService.mjs
+++ src/services/api/Auth/authService.mjs
@@ -1,14 +1,22 @@
 import axios from "axios";
+
 export function useAuthService(){
 
+    // Function to handle user login
     const login = async (email, password) =>{
-        const response = await axios.post('http://localhost:8080/api/auth/login', {email, password}, { withCredentials: true });
-        return response;
+        return await axios.post('http://localhost:8080/api/auth/login', {email, password}, {withCredentials: true});
     }
-    
-    const signup = async (fullName, email, password, confirmPassword) =>{
-        const response = await axios.post('http://localhost:8080/api/auth/signup', {fullName, email, password}, { withCredentials: true });
-        return response;
+
+    // Function to handle user signup
+    const signup = async (fullName, email, password) =>{
+        return await axios.post('http://localhost:8080/api/auth/signup', {
+            fullName,
+            email,
+            password
+        }, { withCredentials: true });
     }
+
+
+
     return {login, signup}
 }

37fde0643765a88d2b9508c083e2c688745cf1d6
Julian Jimenez <julianjimenez2128@gmail.com>
2025-06-27 17:55:53 -0500

    feat: Implement password recovery modal in LoginForm component

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -2,19 +2,27 @@ import { useState} from 'react';
 import {Form, Button, Card} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import {useAuth} from '../../contexts/AuthContext.jsx';
-import {useAuthService} from "../../services/api/Auth/authService.mjs";
+import {useAuthService} from "../../services/api/auth/authService.mjs";
 import {useAlert} from "../../hooks/useAlert.mjs";
-export default function LoginForm() {
+import PasswordRecoveryModal from "../resetPassword/PasswordRecoveryModal.jsx";
+import {useNavigate} from "react-router-dom";
 
+
+export default function LoginForm() {
     // State variables for email and password
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    
+    //State variables and function  for forget password modal
+    const [showForgetPasswordModal, setShowForgetPasswordModal] = useState(false);
+    const hideForgetPasswordModal = () => setShowForgetPasswordModal(false);
 
     // Importing authentication context and service
     const {setAccessToken} = useAuth();// Custom hook for authentication context
     const {login} = useAuthService(); // Custom hook for authentication service
     const { showAlert, AlertComponent} = useAlert(); // Custom hook for alert notifications
 
+    const navigate = useNavigate();
 
     // Handle form submission
     const handleSubmit = async (e) => {
@@ -22,7 +30,7 @@ export default function LoginForm() {
         try {
             const response = await login(email, password);
             setAccessToken(response.data.accessToken); // Set the access token in the authentication context
-            console.log("Login successful, access token:");
+            navigate("/my-trips");
         } catch (err) {
             showAlert("danger","Email or password incorrect.", {autoClose:true, duration:5000} )
         }finally{
@@ -42,6 +50,7 @@ export default function LoginForm() {
                     <Form onSubmit={handleSubmit}>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Control
+                                required
                                 type="email"
                                 placeholder="Email"
                                 value={email}
@@ -51,6 +60,7 @@ export default function LoginForm() {
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Control
+                                required
                                 type="password"
                                 placeholder="Password"
                                 value={password}
@@ -65,7 +75,7 @@ export default function LoginForm() {
                         </div>
                     </Form>
                     <div className="text-center mb-4 mt-3 d-flex justify-content-center align-items-center gap-2">
-                        <Link to="/auth/signup" className="text-decoration-none mr-4">
+                        <Link  onClick={() => setShowForgetPasswordModal(true)} className="text-decoration-none mr-4">
                             ¿Forgot Password?
                         </Link>
                         <Link to="/auth/signup" className="text-decoration-none ms-4">
@@ -74,6 +84,9 @@ export default function LoginForm() {
                     </div>
                 </Card.Body>
             </Card>
+            <>
+                <PasswordRecoveryModal show={showForgetPasswordModal} hide={hideForgetPasswordModal} />
+         </>
         </div>
     );
 }
